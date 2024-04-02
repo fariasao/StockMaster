458c4ba629b324c919f87b2bcf148231339edfc6
fariasao <enzofaarias2010@gmail.com>
2024-04-02 00:21:07 -0300

    feat: Add bean validation

--- src/main/java/br/com/stockmaster/model/Categoria.java
+++ src/main/java/br/com/stockmaster/model/Categoria.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -12,7 +15,12 @@ import lombok.Data;
 public class Categoria {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message = "Nome é obrigatória")
+    @Size(min = 3, max= 255, message = "Nome deve ter pelo menos 3 caracteres")
     private String nome;
+
+    @NotNull
     private String icone;
 
 }
